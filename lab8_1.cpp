d89ac2fe9fbc41ca208e8bb4899151f00aec5a8c
bigloso <59277363+bigloso@users.noreply.github.com>
2020-01-24 16:21:35 +0700

    asfghjk

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 int main(){	
+	float q,w ,total,Interest;
+	float x,y,z,i ;
+	int A=1 ;
+	cout<< "Enter initial loan: ";
+	cin >> x ;
+	cout<<"Enter interest rate per year (%): ";
+	cin >> y ;
+	cout<<"Enter amount you can pay per year: ";
+	cin >> z ;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -15,16 +21,25 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+	while(x == 0){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << setw(13) << left << A; 
+	cout << setw(13) << left << x;
+	Interest = x*y/100;
+	cout << setw(13) << left << Interest;
+	total = x+Interest ;
+	cout << setw(13) << left << total;
+	if(total < z){
+		z=total ;
+		cout << setw(13) << left << z;
+	}
+	else cout << setw(13) << left << z;
+	i=total-z;
+	cout << setw(13) << left << i;
+	i=x;
+	cout << "\n";
+	if(x == 0) break;
+	A++	;
+}
 	return 0;
 }
